fd060963e7a355be842c3620012e6021179da048
lingjie <chenjun2098@126.com>
2024-02-27 17:24:31 +0800

    合理布局代码

--- app/src/main/java/com/wangxingxing/jetpackcomposestate/util/DataGenerators.kt
+++ app/src/main/java/com/wangxingxing/jetpackcomposestate/util/DataGenerators.kt
@@ -10,18 +10,20 @@ import com.wangxingxing.jetpackcomposestate.TodoItem
  * description :
  */
 
+private val msgs = listOf(
+    "Learn compose",
+    "Learn state",
+    "Build dynamic UIs",
+    "Learn Unidirectional Data Flow",
+    "Integrate LiveData",
+    "Integrate ViewModel",
+    "Remember to savedState!",
+    "Build stateless composables",
+    "Use state from stateless composables"
+)
+
+private val icons = TodoIcon.values()
+
 fun generateRandomTodoItem(): TodoItem {
-    val message = listOf(
-        "Learn compose",
-        "Learn state",
-        "Build dynamic UIs",
-        "Learn Unidirectional Data Flow",
-        "Integrate LiveData",
-        "Integrate ViewModel",
-        "Remember to savedState!",
-        "Build stateless composables",
-        "Use state from stateless composables"
-    ).random()
-    val icon = TodoIcon.values().random()
-    return TodoItem(message, icon)
+    return TodoItem(msgs.random(), icons.random())
 }
