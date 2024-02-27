762568f252d83dba0b0d56f0c29d6baa1d4cdbc3
lingjie <chenjun2098@126.com>
2024-02-27 16:54:39 +0800

    单向数据流：ViewModel+LiveData

---
+++ app/src/main/java/com/wangxingxing/jetpackcomposestate/examples/ViewBindingViewModelLiveDataActivity.kt
@@ -0,0 +1,46 @@
+package com.wangxingxing.jetpackcomposestate.examples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.LayoutRes
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.wangxingxing.jetpackcomposestate.databinding.ActivityHelloNormalStateBinding
+
+class ViewBindingViewModelLiveDataActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHelloNormalStateBinding
+    private val viewModel: MyViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHelloNormalStateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 普通方式
+        /*binding.etInput.doAfterTextChanged {
+            binding.tvShow.text = it
+        }*/
+
+        // ViewModel + LiveData
+        binding.etInput.doAfterTextChanged {
+            viewModel.onTextChanged(it.toString())
+        }
+        viewModel.text.observe(this) {
+            binding.tvShow.text = it
+        }
+    }
+}
+
+class MyViewModel : ViewModel() {
+    // backing property（幕后属性）
+    private val _text = MutableLiveData("")
+    // val text: LiveData<...>：LiveData的固定写法，text不能写，要写只能通过改变_text，而约定俗成通过事件（比如onTextChanged）来改变_text。
+    val text: LiveData<String> = _text
+
+    fun onTextChanged(text: String) {
+        _text.value = text
+    }
+}
