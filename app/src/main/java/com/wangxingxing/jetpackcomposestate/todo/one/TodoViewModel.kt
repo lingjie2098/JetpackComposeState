bf70ac2934a6e02ed7c8b701ffd0745034c32d52
lingjie <chenjun2098@126.com>
2024-02-29 09:51:38 +0800

    ViewModel中的状态和事件---来自【Compose状态与状态提升】https://www.bilibili.com/video/BV1ob4y1a7ad?p=31&vd_source=0e526f2b033acee3a3b7fd148889d4a2

--- app/src/main/java/com/wangxingxing/jetpackcomposestate/todo/one/TodoViewModel.kt
+++ app/src/main/java/com/wangxingxing/jetpackcomposestate/todo/one/TodoViewModel.kt
@@ -13,17 +13,20 @@ import com.wangxingxing.jetpackcomposestate.TodoItem
  */
 
 class TodoViewModel : ViewModel() {
-
+    // LingJie's Mark: backing property（幕后属性）
     private val _todoItems = MutableLiveData(listOf<TodoItem>())
+    // LingJie's Mark: 状态
     val todoItems: LiveData<List<TodoItem>> = _todoItems
 
-    // 添加元素
+    // LingJie's Mark: 事件---添加元素
     fun addItem(item: TodoItem) {
+        // LingJie's Mark: 单单新增_todoItems.value集合里面的元素是不行的（不能更新状态），需要把一个新的集合赋值给_todoItems.value。---解释：https://developer.android.google.cn/jetpack/compose/state?hl=zh-cn 中的【可组合项中的状态】最后。
         _todoItems.value = _todoItems.value!! + listOf(item)
     }
 
-    // 移除元素
+    // LingJie's Mark: 事件---移除元素
     fun removeItem(item: TodoItem) {
+        // LingJie's Mark: toMutableList()会转成一个新的MutableList类型对象。_todoItems.value = _todoItems.value!! - listOf(item) 写法也可以。
         _todoItems.value = _todoItems.value!!.toMutableList().also {
             it.remove(item)
         }
